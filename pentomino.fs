34050761585efb747940e62b1018971cb0db99be
Christophe Thibaut <cthibauttof@gmail.com>
2024-10-06 20:41:24 +0200

    demo with 6 shapes

--- pentomino.fs
+++ pentomino.fs
@@ -1,128 +1,137 @@
 : ), ( r,c -- )
-    SWAP , ;
+    15 AND SWAP
+    15 AND 4 LSHIFT
+    OR C, ;
 
 : POINT,
-    5 0 DO 0 , 0 , LOOP ;
+    5 0 DO 0 0 ), LOOP ;
 
 \ categories of pieces
 \ the puzzle can contain only 1 piece per category
 
-HEX
-     1 CONSTANT POINT-1   \ #
-     2 CONSTANT POINT-2   \ #
-     4 CONSTANT POINT-3   \ #
-     8 CONSTANT POINT-4   \ #
-     10 CONSTANT I-LETTER  \ #####
-                           \    #
-     20 CONSTANT L-LETTER  \ ####
-       
-                           \   #
-     40 CONSTANT LOWER-T   \ ####
-    
-                           \   #
-                           \ ###
-     80 CONSTANT S-LETTER  \ #
-    
-                           \   ##
-    100 CONSTANT SNAKE     \ ###
-   
-                           \ # #
-    200 CONSTANT U-LETTER  \ ###
-   
-                           \  ##
-                           \ ##
-    400 CONSTANT BIRD      \  #
-    
-                           \ ###
-                           \  #
-    800 CONSTANT UPPER-T   \  #
-   
-                           \ ##
-                           \  ##
-   1000 CONSTANT STAIRS    \   #
+  1 CONSTANT POINT-1   \ #
+  2 CONSTANT POINT-2   \ #
+  3 CONSTANT POINT-3   \ #
+  4 CONSTANT POINT-4   \ #
+  5 CONSTANT I-LETTER  \ #####
 
-                           \  #
-                           \ ###
-   2000 CONSTANT CROSS     \  #
+                       \    #
+  6 CONSTANT L-LETTER  \ ####
+
+                       \   #
+  7 CONSTANT LOWER-T   \ ####
+
+                       \   #
+                       \ ###
+  8 CONSTANT S-LETTER  \ #
+
+                       \   ##
+ 9 CONSTANT SNAKE      \ ###
+
+                       \ # #
+ 10 CONSTANT U-LETTER  \ ###
+
+                       \  ##
+                       \ ##
+ 11 CONSTANT BIRD      \  #
+
+                       \ ###
+                       \  #
+12 CONSTANT UPPER-T    \  #
+
+                       \ ##
+                       \  ##
+13 CONSTANT STAIRS     \   #
+
+                       \  #
+                       \ ###
+14 CONSTANT CROSS      \  #
 
 
-                           \ #
-                           \ #
-   4000 CONSTANT CORNER    \ ###
+                       \ #
+                       \ #
+15 CONSTANT CORNER     \ ###
 
-                           \ #
-                           \ ##
-   8000 CONSTANT HOUSE     \ ##
+                       \ #
+                       \ ##
+16 CONSTANT HOUSE      \ ##
 
 DECIMAL
 CREATE SHAPES
+HERE
+POINT-1 C,  POINT,
+POINT-2 C,  POINT,
+POINT-3 C,  POINT,
+POINT-4 C,  POINT,
+I-LETTER C, 0 0 ), 0 1 ), 0 2 ), 0 3 ), 0 4 ),              \ #####
 
-POINT-1 ,  POINT,
-POINT-2 ,  POINT,
-POINT-3 ,  POINT,
-POINT-4 ,  POINT,
-I-LETTER , 0 0 ), 0 1 ), 0 2 ), 0 3 ), 0 4 ),              \ #####
+I-LETTER C, 0 0 ), 1 0 ), 2 0 ), 3 0 ), 4 0 ),              \ #
+                                                            \ #
+                                                            \ #
+                                                            \ #
+                                                            \ #
 
-I-LETTER , 0 0 ), 1 0 ), 2 0 ), 3 0 ), 4 0 ),              \ #
-                                                           \ #
-                                                           \ #
-                                                           \ #
-                                                           \ #
+L-LETTER C,  0  0 ),  0  1 ),  0  2 ),  0  3 ), -1  3 ),    \    #
+                                                            \ ####
 
-L-LETTER ,  0  0 ),  0  1 ),  0  2 ),  0  3 ), -1  3 ),    \    #
+L-LETTER C,  0  0 ),   1 0 ),  2  0 ),  3  0 ),  3  1 ),    \ #
+                                                            \ #
+                                                            \ #
+                                                            \ ##
+
+L-LETTER C,  0  0 ),  0  1 ),  0  2 ),  0  3 ),  1  0 ),    \ ####
+                                                            \ #
+
+L-LETTER C,  0  0 ),  0  1 ),  1  1 ),  2  1 ),  3  1 ),    \ ##
+                                                            \  #
+                                                            \  #
+                                                            \  #
+
+L-LETTER C,  0  0 ),  0  1 ),  0  2 ),  0  3 ),  1  3 ),    \ ####
+                                                            \    #
+
+L-LETTER C,  0  0 ),  1  0 ),  2  0 ),  3  0 ),  3 -1 ),    \  #
+                                                            \  #
+                                                            \  #
+                                                            \ ##
+
+L-LETTER C,  0  0 ),  1  0 ),  1  1 ),  1  2 ),  1  3 ),   \ #
                                                            \ ####
 
-L-LETTER ,  0  0 ),   1 0 ),  2  0 ),  3  0 ),  3  1 ),    \ #
+L-LETTER C,  0  0 ),  0  1 ),  1  0 ),  2  0 ),  3  0 ),   \ ##
                                                            \ #
                                                            \ #
-                                                           \ ##
-
-L-LETTER ,  0  0 ),  0  1 ),  0  2 ),  0  3 ),  1  0 ),    \ ####
                                                            \ #
 
-L-LETTER ,  0  0 ),  0  1 ),  1  1 ),  2  1 ),  3  1 ),    \ ##
-                                                           \  #
-                                                           \  #
-                                                           \  #
-
-L-LETTER ,  0  0 ),  0  1 ),  0  2 ),  0  3 ),  1  3 ),    \ ####
-                                                           \    #
-
-I-LETTER ,  0  0 ),  1  0 ),  2  0 ),  3  0 ),  3 -1 ),    \  #
-                                                           \  #
-                                                           \  #
-                                                           \ ##
-
-I-LETTER ,  0  0 ),  1  0 ),  1  1 ),  1  2 ),  1  3 ),    \ #
-                                                           \ ####
-
+HERE SWAP - 6 / CONSTANT MAX-SHAPE
 : NTH-SHAPE ( n -- addr )
-    11 CELLS * SHAPES + ;
+    6 * SHAPES + ;
 
-: .BLOCK ( x,y,c -- )
-    \ -ROT AT-XY EMIT ;
-    -ROT SWAP . . EMIT CR ;
+HEX FFFFFFFFFFFFFFF0 CONSTANT NEG-MASK DECIMAL
+: NIBBLE>COORD ( b -- n )
+    15 AND DUP 8 AND IF NEG-MASK OR THEN ;
+
+: BYTE>XY ( b -- x,y )
+    DUP   NIBBLE>COORD
+    SWAP  4 RSHIFT NIBBLE>COORD ;
+
+: .BLOCK ( c,x,y -- )
+AT-XY EMIT ;
 
 : ADD-COORDS ( x,y,r,c -- x+c,y+r )
     -ROT + -ROT + SWAP ;
 
 : .SHAPE ( x,y,c,addr -- )
-    CELL+ DUP 10 CELLS + SWAP DO
-        I 2@
-        >R 2DUP ADD-COORDS
-        R@ .BLOCK
-        R>
-        2 CELLS
-    +LOOP
+    1+ DUP 5 + SWAP DO
+        I C@
+        2OVER
+        ROT BYTE>XY
+        ADD-COORDS
+        ROT DUP
+        2SWAP .BLOCK
+    LOOP
     DROP 2DROP ;
 
-: DUMP-SHAPE
-    11 CELLS * SHAPES + 11 CELLS DUMP ;
-
-: DEMO
-    5 0 DO I DUMP-SHAPE key drop cr LOOP ;
-
-DEMO
-    \ PAGE 10 10 42 5 NTH-SHAPE dbg .SHAPE 
-
+: DEMO 
+    MAX-SHAPE 0 DO PAGE I . 10 10 64 I NTH-SHAPE DUP C@ ROT + SWAP .SHAPE CR KEY DROP LOOP ;
 
