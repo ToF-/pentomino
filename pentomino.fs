a37e0380c997a07a6772f38b5a50e21a8f2aa883
Christophe Thibaut <cthibauttof@gmail.com>
2024-10-06 19:14:58 +0200

    storing shapes

--- pentomino.fs
+++ pentomino.fs
@@ -1,68 +1,128 @@
-
-
-: ROW-COL>COORD ( r,c -- ch )
-    SWAP 4 LSHIFT OR ;
-
-: COORD>ROW-COL ( ch -- r,c )
-    DUP 4 RSHIFT SWAP 15 AND ;
-
 : ), ( r,c -- )
-    ROW-COL>COORD C, ;
+    SWAP , ;
 
-128 CONSTANT POINT
 : POINT,
-    POINT C, 0 C, 0 C, 0 C, 0 C, ;
+    5 0 DO 0 , 0 , LOOP ;
 
-CREATE PIECES
-POINT, POINT, POINT, POINT,
-0 0 ),  0 1 ),  0 2 ),  0 3 ),  1 3 ),
-0 0 ),  0 1 ),  1 0 ),  1 1 ),  2 1 ),
-0 0 ),  1 0 ),  1 1 ),  1 2 ),  2 0 ),
-0 1 ),  1 0 ),  1 1 ),  1 2 ),  2 1 ),
-0 0 ),  0 2 ),  1 0 ),  1 1 ),  1 2 ),
-0 0 ),  1 0 ),  2 0 ),  3 0 ),  4 0 ),
-0 0 ),  1 0 ),  2 0 ),  2 1 ),  2 2 ),
-0 0 ),  1 0 ),  1 1 ),  1 2 ),  2 2 ),
-0 1 ),  1 0 ),  1 1 ),  1 2 ),  1 3 ),
-0 1 ),  1 0 ),  1 1 ),  2 1 ),  2 2 ),
-0 0 ),  1 0 ),  1 1 ),  2 1 ),  2 2 ),
-0 0 ),  1 0 ),  1 1 ),  2 1 ),  2 1 ),
+\ categories of pieces
+\ the puzzle can contain only 1 piece per category
 
-: NTH-PIECE ( n -- addr )
-    5 * PIECES + ;
+HEX
+     1 CONSTANT POINT-1   \ #
+     2 CONSTANT POINT-2   \ #
+     4 CONSTANT POINT-3   \ #
+     8 CONSTANT POINT-4   \ #
+     10 CONSTANT I-LETTER  \ #####
+                           \    #
+     20 CONSTANT L-LETTER  \ ####
+       
+                           \   #
+     40 CONSTANT LOWER-T   \ ####
+    
+                           \   #
+                           \ ###
+     80 CONSTANT S-LETTER  \ #
+    
+                           \   ##
+    100 CONSTANT SNAKE     \ ###
+   
+                           \ # #
+    200 CONSTANT U-LETTER  \ ###
+   
+                           \  ##
+                           \ ##
+    400 CONSTANT BIRD      \  #
+    
+                           \ ###
+                           \  #
+    800 CONSTANT UPPER-T   \  #
+   
+                           \ ##
+                           \  ##
+   1000 CONSTANT STAIRS    \   #
 
-: .(COORD) ( x,y -- )
-    ." (" SWAP . ." ," . ." )" ;
+                           \  #
+                           \ ###
+   2000 CONSTANT CROSS     \  #
 
-: .(SHARP) ( x,y -- )
-    AT-XY 35 EMIT ;
 
-: .(PIVOT) ( x,y -- )
-    AT-XY ." ▓" ;
+                           \ #
+                           \ #
+   4000 CONSTANT CORNER    \ ###
 
-: .(POINT) ( x,y -- )
-    AT-XY ." ▒" ;
+                           \ #
+                           \ ##
+   8000 CONSTANT HOUSE     \ ##
 
-: .(PIECE) ( x,y,addr -- )
-    DUP 5 + SWAP DO
-        2DUP I C@ COORD>ROW-COL 
-        2DUP 0 0 D= >R
-        ROT + -ROT +
-        R> IF .(PIVOT) ELSE .(POINT) THEN 
-    LOOP 2DROP ;
+DECIMAL
+CREATE SHAPES
 
-: .PIECE ( x,y,addr -- )
-    DUP C@ 128 AND IF
-        DROP .(PIVOT)
-    ELSE
-        .(PIECE)
-    THEN ;
+POINT-1 ,  POINT,
+POINT-2 ,  POINT,
+POINT-3 ,  POINT,
+POINT-4 ,  POINT,
+I-LETTER , 0 0 ), 0 1 ), 0 2 ), 0 3 ), 0 4 ),              \ #####
+
+I-LETTER , 0 0 ), 1 0 ), 2 0 ), 3 0 ), 4 0 ),              \ #
+                                                           \ #
+                                                           \ #
+                                                           \ #
+                                                           \ #
+
+L-LETTER ,  0  0 ),  0  1 ),  0  2 ),  0  3 ), -1  3 ),    \    #
+                                                           \ ####
+
+L-LETTER ,  0  0 ),   1 0 ),  2  0 ),  3  0 ),  3  1 ),    \ #
+                                                           \ #
+                                                           \ #
+                                                           \ ##
+
+L-LETTER ,  0  0 ),  0  1 ),  0  2 ),  0  3 ),  1  0 ),    \ ####
+                                                           \ #
+
+L-LETTER ,  0  0 ),  0  1 ),  1  1 ),  2  1 ),  3  1 ),    \ ##
+                                                           \  #
+                                                           \  #
+                                                           \  #
+
+L-LETTER ,  0  0 ),  0  1 ),  0  2 ),  0  3 ),  1  3 ),    \ ####
+                                                           \    #
+
+I-LETTER ,  0  0 ),  1  0 ),  2  0 ),  3  0 ),  3 -1 ),    \  #
+                                                           \  #
+                                                           \  #
+                                                           \ ##
+
+I-LETTER ,  0  0 ),  1  0 ),  1  1 ),  1  2 ),  1  3 ),    \ #
+                                                           \ ####
+
+: NTH-SHAPE ( n -- addr )
+    11 CELLS * SHAPES + ;
+
+: .BLOCK ( x,y,c -- )
+    \ -ROT AT-XY EMIT ;
+    -ROT SWAP . . EMIT CR ;
+
+: ADD-COORDS ( x,y,r,c -- x+c,y+r )
+    -ROT + -ROT + SWAP ;
+
+: .SHAPE ( x,y,c,addr -- )
+    CELL+ DUP 10 CELLS + SWAP DO
+        I 2@
+        >R 2DUP ADD-COORDS
+        R@ .BLOCK
+        R>
+        2 CELLS
+    +LOOP
+    DROP 2DROP ;
+
+: DUMP-SHAPE
+    11 CELLS * SHAPES + 11 CELLS DUMP ;
 
 : DEMO
-    16 0 DO
-        PAGE I . CR
-        25 25 I NTH-PIECE .PIECE
-        CR CR CR
-        KEY DROP
-    LOOP ;
+    5 0 DO I DUMP-SHAPE key drop cr LOOP ;
+
+DEMO
+    \ PAGE 10 10 42 5 NTH-SHAPE dbg .SHAPE 
+
 
