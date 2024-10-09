abae3e4d941bbd3e8f9c446abafd7b4fcc9ed542
Christophe Thibaut <cthibauttof@gmail.com>
2024-10-09 16:45:31 +0200

    place and place?

--- pentomino.fs
+++ pentomino.fs
@@ -8,21 +8,34 @@ REQUIRE puzzle.fs
     >R TRUE -ROT R>
     OVER 5 + ROT DO                     \ 1,puz,xy
         2DUP I C@                       \ 1,puz,xy,puz,xy,ij
-        TRANSLATE? IF PUZZLE-XY-FIT? IF
+        TRANSLATE? IF                   \ 1,puz,xy,puz,lk
+            PUZZLE-XY-FIT? IF           \ 1,puz,xy
+            ELSE
+                ROT DROP FALSE -ROT
+                LEAVE
+            THEN
         ELSE
-            ROT DROP FALSE -ROT
+            DROP ROT DROP FALSE -ROT
             LEAVE
-        THEN THEN
+        THEN
     LOOP
     2DROP ;
 
 : PLACE ( puz,pos,xy -- f)
     OVER 5 + ROT DO
-        2DUP I C@ TRANSLATE? IF 
+        2DUP I C@ TRANSLATE? IF
             PUZZLE-XY!
             -ROT NIP
         THEN
     LOOP DROP ;
+
+: PLACE? ( puz,pos,xy -- puz',1| 0 )
+    ROT DUP >R -ROT 2DUP R> -ROT
+    CAN-FIT? IF
+        PLACE TRUE
+    ELSE
+        2DROP DROP FALSE
+    THEN ;
 
 : .PUZZLE ( puz -- )
     8 0 DO
