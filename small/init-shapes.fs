f095c25d0c55f06ccd943b32aa08c93efab8e34d
Christophe Thibaut <cthibauttof@gmail.com>
2024-10-11 12:09:00 +0200

    coords for a shape position

--- small/init-shapes.fs
+++ small/init-shapes.fs
@@ -2,6 +2,7 @@
 
 5 CONSTANT PIECE-LENGTH
 25 CONSTANT PIECE-AREA
+CHAR # CONSTANT PIECE-BLOCK 
 
 : SHAPE ( n <name> -- )
     CREATE C, ;
@@ -10,3 +11,16 @@
     [CHAR] | WORD
     COUNT OVER + SWAP
     DO I C@ C, LOOP ;
+
+: POSITION ( piece,n -- addr )
+    PIECE-AREA * SWAP 1+ + ;
+
+: COORDS ( addr -- x4,y4,x3,y3,x2,y2,x1,y1,x0,y0 )
+    PIECE-AREA 1-
+    PIECE-AREA 0 DO
+       OVER PIECE-AREA + I - 1-
+       C@ PIECE-BLOCK = IF
+           DUP PIECE-LENGTH /MOD 2SWAP
+       THEN
+        1-
+    LOOP 2DROP ;
--- small/test-init-shapes.fs
+++ small/test-init-shapes.fs
@@ -3,6 +3,7 @@
 REQUIRE ffl/tst.fs
 REQUIRE init-shapes.fs
 
+PAGE
 ." constants" CR
 T{
     5 PIECE-LENGTH ?S
@@ -24,6 +25,16 @@ T{
     MY-SHAPE 3 + C@  CHAR # ?S
     MY-SHAPE 5 + C@  CHAR . ?S
     MY-SHAPE 6 + C@  CHAR # ?S
+}T
+
+." extracting shape coords for a position" CR
+T{
+    MY-SHAPE 0 POSITION COORDS
+    0 ?S  0 ?S
+    0 ?S  2 ?S
+    1 ?S  0 ?S
+    1 ?S  1 ?S
+    1 ?S  2 ?S
 }T
 BYE
 
