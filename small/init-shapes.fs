6ffd16c736ddfc85d2f5e364eb63e29cc6ad62f4
Christophe Thibaut <cthibauttof@gmail.com>
2024-10-11 12:46:51 +0200

    calibrate and coords

--- small/init-shapes.fs
+++ small/init-shapes.fs
@@ -2,7 +2,8 @@
 
 5 CONSTANT PIECE-LENGTH
 25 CONSTANT PIECE-AREA
-CHAR # CONSTANT PIECE-BLOCK 
+CHAR # CONSTANT PIECE-BLOCK
+CHAR . CONSTANT PIECE-EMPTY
 
 : SHAPE ( n <name> -- )
     CREATE C, ;
@@ -24,3 +25,44 @@ CHAR # CONSTANT PIECE-BLOCK
        THEN
         1-
     LOOP 2DROP ;
+
+: FIRST-COORDS ( addr -- x,y )
+    PIECE-AREA 0 DO
+        DUP I + C@ PIECE-BLOCK = IF
+            I PIECE-LENGTH /MOD
+            LEAVE
+        THEN
+    LOOP ROT DROP ;
+
+: TRANSLATE ( x0,y0,x1,y1 -- x0+x1,y0+y1 )
+    ROT + -ROT + SWAP ;
+
+: OPPOSE ( x,y -- -x,-y )
+    NEGATE SWAP NEGATE SWAP ;
+
+: WITHIN-PIECE-AREA? ( x,y -- f )
+    0 PIECE-LENGTH WITHIN
+    SWAP 0 PIECE-LENGTH WITHIN AND ;
+
+: COPY-SHAPE-TO-PAD ( addr )
+    PAD PIECE-AREA CMOVE ;
+
+: EMPTY-SHAPE ( addr )
+    PIECE-AREA PIECE-EMPTY FILL ;
+
+: CALIBRATE ( addr -- )
+    DUP FIRST-COORDS OPPOSE ROT
+    DUP COPY-SHAPE-TO-PAD
+    DUP EMPTY-SHAPE
+    PIECE-AREA 0 DO
+        PAD I + C@ PIECE-BLOCK = IF
+            -ROT 2DUP I PIECE-LENGTH /MOD
+            TRANSLATE 2DUP WITHIN-PIECE-AREA? IF
+                PIECE-LENGTH * +
+                >R ROT DUP R> +
+                PIECE-BLOCK SWAP C!
+            ELSE
+                2DROP
+            THEN
+        THEN
+    LOOP DROP 2DROP ;
--- small/test-init-shapes.fs
+++ small/test-init-shapes.fs
@@ -13,23 +13,44 @@ T{
 ." compiling a shape" CR
 T{
     8 SHAPE MY-SHAPE
-    | #.#..|
-    | ###..|
     | .....|
     | .....|
+    | .#.#.|
+    | .###.|
     | .....|
 
     MY-SHAPE C@ 8 ?S
-    MY-SHAPE 1 + C@  CHAR # ?S
-    MY-SHAPE 2 + C@  CHAR . ?S
-    MY-SHAPE 3 + C@  CHAR # ?S
-    MY-SHAPE 5 + C@  CHAR . ?S
-    MY-SHAPE 6 + C@  CHAR # ?S
+    MY-SHAPE 0 POSITION
+    DUP 11 + C@  CHAR # ?S
+    DUP 12 + C@  CHAR . ?S
+    DUP 13 + C@  CHAR # ?S
+    DUP 16 + C@  CHAR # ?S
+    DUP 17 + C@  CHAR # ?S
+    DUP 18 + C@  CHAR # ?S
+    DROP
 }T
 
-." extracting shape coords for a position" CR
+." finding first coords in a shape position" CR
 T{
-    MY-SHAPE 0 POSITION COORDS
+    MY-SHAPE 0 POSITION
+    FIRST-COORDS SWAP 1 ?S 2 ?S
+}T
+
+." calibrating a shape position" CR
+T{
+    MY-SHAPE 0 POSITION CALIBRATE
+    MY-SHAPE 0 POSITION
+    DUP 0 + C@  CHAR # ?S
+    DUP 1 + C@  CHAR . ?S
+    DUP 2 + C@  CHAR # ?S
+    DUP 5 + C@  CHAR # ?S
+    DUP 6 + C@  CHAR # ?S
+    DUP 7 + C@  CHAR # ?S
+    DROP
+}T
+." extracting shape coords for a position, calibrated" CR
+T{
+    MY-SHAPE 0 POSITION DUP CALIBRATE COORDS
     0 ?S  0 ?S
     0 ?S  2 ?S
     1 ?S  0 ?S
