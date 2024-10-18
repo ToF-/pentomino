58264da0e761f4c94900ee5f5afb244f4ad99692
Christophe Thibaut <cthibauttof@gmail.com>
2024-10-18 17:37:01 +0200

    board

---
+++ medium/board.fs
@@ -0,0 +1,40 @@
+\ board.fs
+
+0 CONSTANT EMPTY-BOARD
+
+: BOARD<XY! ( bd,x,y -- bd' )
+    8 * + 1 SWAP LSHIFT OR ;
+
+\ 00 01 02  → 40
+\ 08          48
+\             56 57
+
+: BOARD>XY ( board,n -- x,y,f )
+    DUP 1 SWAP LSHIFT ROT AND IF
+        8 /MOD TRUE
+    ELSE
+        -1 -1 FALSE
+    THEN ;
+
+: SQUARE-ROTATE ( x,y -- y,7-x )
+    SWAP 7 SWAP - ;
+
+: .BOARD-COORDS ( bd -- )
+    64 0 DO
+        1 I LSHIFT OVER AND IF
+            I 8 /MOD SWAP ." (" 0 .R ." ," 0 .R ." ) "
+        THEN
+    LOOP DROP ;
+
+: BOARD-ROTATE ( bd -- bd' )
+    EMPTY-BOARD SWAP
+    64 0 DO
+        DUP I BOARD>XY IF
+            SQUARE-ROTATE
+            2>R SWAP 2R>
+            BOARD<XY!
+            SWAP
+        ELSE
+            2DROP DROP
+        THEN
+    LOOP DROP ;
--- medium/tests.fs
+++ medium/tests.fs
@@ -2,6 +2,7 @@
 
 PAGE
 REQUIRE test-pieces.fs
+REQUIRE test-board.fs
 REQUIRE test-bitfields.fs
 REQUIRE test-situation.fs
 
