16c6342eaac169cf005f85b41717e392609cfa12
Christophe Thibaut <cthibauttof@gmail.com>
2024-11-09 15:50:14 +0100

    rotate

--- simplify/code/shape.fs
+++ simplify/code/shape.fs
@@ -24,4 +24,44 @@ CHAR # CONSTANT SQUARE
 : SHAPE| ( ccccc | -- coords )
     COL OFF ROW OFF 0 | ;
 
+CREATE SHAPE-COORDS 10 CELLS ALLOT
+
+: NTH-COORDS@ ( n -- x,y )
+    2* CELLS SHAPE-COORDS + 2@ SWAP ;
+
+: NTH-COORDS! ( x,y,n -- )
+    2* CELLS SHAPE-COORDS + 2! ;
+
+: GET-SHAPE-COORDS ( n -- )
+    5 0 DO
+        DUP 5 I 1+ -
+        8 * RSHIFT 255 AND
+        DUP 15 AND SWAP 4 RSHIFT
+        I NTH-COORDS!
+    LOOP DROP ;
+
+: ROTATE-COORD ( x,y -- -y,x )
+    NEGATE SWAP ;
+
+: MIN-COORDS ( -- x,y )
+    5 5
+    5 0 DO
+        I NTH-COORDS@
+        ROT MIN -ROT MIN SWAP
+    LOOP ;
+
+: CALIBRATE
+    MIN-COORDS
+    NEGATE SWAP NEGATE SWAP
+    5 0 DO
+        I NTH-COORDS@
+        2OVER ROT + -ROT + SWAP
+        I NTH-COORDS!
+    LOOP 2DROP ;
+
+: ROTATE-SHAPE-COORDS
+    5 0 DO
+        I NTH-COORDS@ ROTATE-COORD I NTH-COORDS!
+    LOOP
+    CALIBRATE ;
 
--- simplify/code/test-shape.fs
+++ simplify/code/test-shape.fs
@@ -3,7 +3,7 @@
 REQUIRE ffl/tst.fs
 REQUIRE shape.fs
 
-." acquiring a shape coordinates"
+." acquiring a shape" CR
 T{
 
 HEX
@@ -19,6 +19,27 @@ SHAPE| ##.|
      | .#.|
 
 0010112112 ?S
+}T
+DECIMAL
+." getting coordinates" CR
+T{
+SHAPE| ##.|
+     | .##|
+     | .#.|
+
+GET-SHAPE-COORDS
+0 NTH-COORDS@ SWAP 0 ?S 0 ?S
+1 NTH-COORDS@ SWAP 1 ?S 0 ?S
+2 NTH-COORDS@ SWAP 1 ?S 1 ?S
+3 NTH-COORDS@ SWAP 2 ?S 1 ?S
+4 NTH-COORDS@ SWAP 1 ?S 2 ?S
+ROTATE-SHAPE-COORDS
+0 NTH-COORDS@ SWAP 2 ?S 0 ?S         \ ..#
+1 NTH-COORDS@ SWAP 2 ?S 1 ?S         \ ###
+2 NTH-COORDS@ SWAP 1 ?S 1 ?S         \ .#.
+3 NTH-COORDS@ SWAP 1 ?S 2 ?S
+4 NTH-COORDS@ SWAP 0 ?S 1 ?S
+
 }T
 CR
 BYE
