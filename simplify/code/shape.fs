4596dd790c7fb6626fc9f700ca955dd93e6b9de7
Christophe Thibaut <cthibauttof@gmail.com>
2024-11-15 23:35:12 +0100

    refactoring

--- simplify/code/shape.fs
+++ simplify/code/shape.fs
@@ -4,6 +4,7 @@ CHAR | CONSTANT BAR CHAR # CONSTANT SQUARE
 -16 CONSTANT EXPAND-MASK 15 CONSTANT VALUE-MASK
 8 CONSTANT NEGATIVE-MASK 4 CONSTANT NIBBLE
 7 CONSTANT MAX-NIBBLE 0 CONSTANT EMPTY-COORDS
+5 CONSTANT NBCOORDS
 VARIABLE COL VARIABLE ROW
 
 : SHAPE<<N ( n,coords -- coords' )
@@ -15,35 +16,38 @@ VARIABLE COL VARIABLE ROW
 : SHAPE<<XY ( x,y,coords -- coords' )
     SHAPE<<N SHAPE<<N ;
 
+: ++! ( addr -- )
+    1 SWAP +! ;
+
 : | ( ccccc | coords -- coords' )
     BAR WORD
     COUNT OVER + SWAP DO
         I C@ SQUARE = IF
             COL @ ROW @ ROT SHAPE<<XY
         THEN
-        1 COL +!
+        COL ++!
     LOOP
     COL OFF
-    1 ROW +! ;
+    ROW ++! ;
 
 : NEW-SHAPE ( ccccc | -- coords )
     COL OFF ROW OFF EMPTY-COORDS ;
 
 : SHAPE>>N  ( coords -- n,coord' )
     DUP VALUE-MASK AND EXPAND
-    SWAP 4 RSHIFT ;
+    SWAP NIBBLE RSHIFT ;
 
 : SHAPE>>XY ( coords -- x,y,coords' )
     SHAPE>>N SHAPE>>N ;
 
 : XYS ( coords -- x0,y0..x4,y4 )
-    5 0 DO SHAPE>>XY LOOP DROP ;
+    NBCOORDS 0 DO SHAPE>>XY LOOP DROP ;
 
 : XY-MIN ( x,y,i,j -- k,l )
     ROT MIN -ROT MIN SWAP ;
 
 : MINIMUM-XY ( coords -- x,y )
-    XYS MAX-NIBBLE MAX-NIBBLE 5 0 DO XY-MIN LOOP ;
+    XYS MAX-NIBBLE MAX-NIBBLE NBCOORDS 0 DO XY-MIN LOOP ;
 
 : XY-NEGATE ( x,y -- -x,-y )
     NEGATE SWAP NEGATE SWAP ;
@@ -53,7 +57,7 @@ VARIABLE COL VARIABLE ROW
 
 : (TRANSLATE) ( coords,x,y -- coords' )
     ROW ! COL ! XYS
-    0 5 0 DO
+    EMPTY-COORDS NBCOORDS 0 DO
         -ROT COL @ ROW @ XY-ADD
         ROT SHAPE<<XY
     LOOP ;
@@ -68,14 +72,14 @@ VARIABLE COL VARIABLE ROW
     DUP MINIMUM-XY XY-NEGATE (TRANSLATE) ;
 
 : ROTATE ( coords -- coords' )
-    XYS 0 5 EMPTY-COORDS DO
+    XYS EMPTY-COORDS NBCOORDS 0 DO
         -ROT XY-ROTATE
         ROT SHAPE<<XY
         LOOP
     (CALIBRATE) ;
 
 : FLIP ( coords -- coords' )
-    XYS 0 5 EMPTY-COORDS DO
+    XYS EMPTY-COORDS NBCOORDS 0 DO
         -ROT XY-FLIP
         ROT SHAPE<<XY
     LOOP
